eda45f8f3993714fedeec5b4fdf0417cf9ba09d0
MaFe1401 <marcferre42@gmail.com>
2021-05-07 20:00:11 +0200

    database

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export async function startDatabase() {
-    const db = await mongoose.connect('mongodb://mongo:27017/looping', {
+    const db = await mongoose.connect('mongodb://database:27017/looping', {
         useNewUrlParser: true,
         useFindAndModify: false
     });
