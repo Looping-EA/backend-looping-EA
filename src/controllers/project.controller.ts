0c7394c365532fa781da082333366caa2198af1c
MaFe1401 <marcferre42@gmail.com>
2021-05-07 22:09:27 +0200

    updated project controller

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -3,5 +3,6 @@ import Project from '../models/Project';
 
 export async function returnProjects(req:Request, res:Response){
     let projects = await Project.find();
+    projects.forEach((project)=>project.populate('chats').populate('teams').populate('tasks').populate('collaboration').populate('owners'));
     res.status(201).json(projects);
 }
