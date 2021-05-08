c0b3aa8df241202ad4aa747f3c27d3f12e7cf3dd
MaFe1401 <marcferre42@gmail.com>
2021-05-08 18:22:39 +0200

    add project

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -4,5 +4,27 @@ import Project from '../models/Project';
 export async function returnProjects(req:Request, res:Response){
     let projects = await Project.find();
     projects.forEach((project)=>project.populate('chats').populate('teams').populate('tasks').populate('collaboration').populate('owners'));
+    console.log("projects returned");
     res.status(201).json(projects);
+}
+export async function addProject(req:Request, res:Response){
+    const {name, chats, creationDate, teams, tasks, description, collaboration, owners}=req.body;
+    const project_compr = await Project.findOne({'name': name});
+    if (!project_compr){
+        const newProject={
+            name:name,
+            chats:chats,
+            creationDate:creationDate,
+            teams:teams,
+            tasks:tasks,
+            description:description,
+            collaboration:collaboration,
+            owners:owners
+        }
+        const project = new Project(newProject);
+        await project.save();
+        return res.status(201).json(project.toJSON());
+    }
+    else return res.status(401).json({message: 'The name already exists'});
+
 }
--- src/routes/project_service.ts
+++ src/routes/project_service.ts
@@ -1,9 +1,11 @@
 import {Router} from 'express';
-import {returnProjects} from '../controllers/project.controller'
+import {returnProjects, addProject} from '../controllers/project.controller'
 
 const project_router = Router();
 
 project_router.route('/projects/')
     .post(returnProjects)
 
+project_router.route('/projects/add')
+    .post(addProject)
 export default project_router;
